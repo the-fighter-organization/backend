144db718c62e630c2f31c5b2e949ac765a58d165
Igor Bueno <igor_mcb@hotmail.com>
2019-07-29 14:53:51 -0400

    Sincronizando para nova branch de faturas

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import AuthConfig from './config/authentication';
 const app = express();
 
 // middlewares do express
-app.use(cors({origin:['http://localhost:3000']}))
+app.use(cors({origin:['http://localhost:3000', 'http://warrior-frontend.appspot.com']}))
 app.use(bodyParser.json({limit : '5mb'}))
 app.use(bodyParser.urlencoded({extended:true, limit : '50mb'}));
 app.use(morgan('combined'));
