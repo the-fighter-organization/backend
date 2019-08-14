f832069834be358507d6500548ee9a5bb33466cc
Igor Bueno <igor_mcb@hotmail.com>
2019-08-14 19:26:45 -0400

    Inicializando swagger

--- src/config/controller.ts
+++ src/config/controller.ts
@@ -2,10 +2,13 @@ import UsuarioController from '../controllers/usuarios';
 import * as express from 'express';
 import AlunoController from '../controllers/alunos';
 import HomeController from '../controllers/home';
+import { serve, setup } from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 
-export default class ConfigConfig{
-    public static config(app:express.Express){
+export default class ConfigConfig {
+    public static config(app: express.Express) {
         app.use('/', HomeController.config())
+        app.use('/api-docs', serve, setup(swaggerDocument))
         app.use('/usuarios', UsuarioController.config())
         app.use('/alunos', AlunoController.config())
     }
